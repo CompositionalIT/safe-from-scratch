90b4fcb6ef948886bb872b3b24d3e5a9d21ecdc0
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-19 17:40:09 +0000

    Server side - Basic Giraffe

---
+++ server/Program.fs
@@ -0,0 +1,9 @@
+﻿open Microsoft.Extensions.Configuration
+open Giraffe
+open Microsoft.AspNetCore.Builder
+
+let b = WebApplication.CreateBuilder()
+b.Services.AddGiraffe() |> ignore
+let app = b.Build()
+app.UseGiraffe(text "Hello world")
+app.Run()
