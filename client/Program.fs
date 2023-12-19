1275ac5bf2e902e73acad14912ce1d537cbe7cfe
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-19 18:11:18 +0000

    Tiny cleanup

--- client/Program.fs
+++ client/Program.fs
@@ -1,13 +1,14 @@
 ﻿open Fable.SimpleHttp
 open Browser.Types
+open Browser.Dom
 
-let btnFetch = Browser.Dom.document.getElementById "btnFetch" :?> HTMLButtonElement
+let btnFetch = document.getElementById "btnFetch" :?> HTMLButtonElement
 
 btnFetch.onclick <-
     fun _ ->
         async {
             let! code, data = Http.get "/api/data"
-            Browser.Dom.console.log (code, data)
-            Browser.Dom.window.alert $"Data received: Response code {code}, '{data}'"
+            console.log (code, data)
+            window.alert $"Data received: Response code {code}, '{data}'"
         }
         |> Async.StartImmediate
