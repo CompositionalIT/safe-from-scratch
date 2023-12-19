a14a7c9ca117da3b00da4187c6a68bd1cba0b5a3
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-19 18:08:28 +0000

    Client: Server communication

--- client/Program.fs
+++ client/Program.fs
@@ -1,1 +1,13 @@
-﻿printfn "Hello from F# in the browser!"
+﻿open Fable.SimpleHttp
+open Browser.Types
+
+let btnFetch = Browser.Dom.document.getElementById "btnFetch" :?> HTMLButtonElement
+
+btnFetch.onclick <-
+    fun _ ->
+        async {
+            let! code, data = Http.get "/api/data"
+            Browser.Dom.console.log (code, data)
+            Browser.Dom.window.alert $"Data received: Response code {code}, '{data}'"
+        }
+        |> Async.StartImmediate
