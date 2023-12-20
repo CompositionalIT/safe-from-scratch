1409820300e2b09db11003c077ff3e2c82f6d9d2
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-20 00:10:23 +0000

    Add Feliz w/ F# React component.

--- client/Program.fs
+++ client/Program.fs
@@ -1,14 +1,20 @@
 ﻿open Fable.SimpleHttp
-open Browser.Types
 open Browser.Dom
+open Feliz
 
-let btnFetch = document.getElementById "btnFetch" :?> HTMLButtonElement
+[<ReactComponent>]
+let MyComponent () =
+    Html.button [
+        prop.id "btnFetch"
+        prop.text "Fetch Data!"
+        prop.onClick (fun _ ->
+            (async {
+                let! code, data = Http.get "/api/data"
+                console.log (code, data)
+                window.alert $"Data received: Response code {code}, '{data}'"
+             }
+             |> Async.StartImmediate))
+    ]
 
-btnFetch.onclick <-
-    fun _ ->
-        async {
-            let! code, data = Http.get "/api/data"
-            console.log (code, data)
-            window.alert $"Data received: Response code {code}, '{data}'"
-        }
-        |> Async.StartImmediate
+let root = ReactDOM.createRoot (document.getElementById "root")
+root.render (MyComponent())
